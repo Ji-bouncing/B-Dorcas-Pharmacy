4123e8488877c8817a62af799faf84fb037e76d5
Ji-bouncing <abdullah2002@gmail.com>
2024-06-12 15:14:28 +0100

    Third Commit on this project

--- main.js
+++ main.js
@@ -6,7 +6,7 @@
     const password = document.querySelector("#pass").value;
     if( email == "ajibolaabdullah2002@gmail.com" && password == "12345678"){
         alert('Login successful');
-        window.location.assign('/login.html');
+        window.location.assign('/index.html');
     }else{
         alert('Wrong Entry Inavlid Details');
         return;
